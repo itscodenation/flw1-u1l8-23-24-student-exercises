ec095868d776a20b091080dcaa65044a165785cb
CN-Curriculum <138328232+CN-Curriculum@users.noreply.github.com>
2023-09-08 15:26:48 -0400

    Add lesson 8 exercise

---
+++ createAppend.js
@@ -0,0 +1,33 @@
+// html elements
+let body = document.querySelector("body");
+let container = document.querySelector(".container");
+
+// 1. Create an h2 element.
+// - Store it in a variable called h2.
+
+
+// 2. Set the innerHTML of h2 to your name.
+
+
+
+// 3. Select the container div and append the h2 element.
+
+
+
+// 4. Create a paragraph element.
+// - Store it in a variable called para.
+
+
+// 5. Set the innerHTML of para to a fact about yourself.
+
+
+// 6. Select the container div and append the paragraph element.
+
+
+
+// Uncomment the code below after completing 1-6.
+// Where do you see this on the page?
+// Why is it located there even though the body is selected?
+// let h1 = document.createElement('h1');
+// h1.innerHTML = "My Webpage";
+// body.appendChild(h1);
---
+++ jsLab.js
@@ -0,0 +1,65 @@
+// html elements
+let tile1 = document.querySelector('.tile-1')
+let tile2 = document.querySelector('.tile-2')
+let tile3 = document.querySelector('.tile-3')
+let tile4 = document.querySelector('.tile-4')
+let tile5 = document.querySelector('.tile-5')
+let tile6 = document.querySelector('.tile-6')
+let tile7 = document.querySelector('.tile-7')
+let tile8 = document.querySelector('.tile-8')
+let tile9 = document.querySelector('.tile-9')
+
+
+/********* NOTE: unsure which tile is which? Be sure to reference the HTML when you need to double-check! ************/
+
+
+// 1. Tile 1 feels a little sad. 
+// - Update the inner HTML to show an encouraging message! 
+// - Something like "YOU GOT THIS!" or even a "You're a beautiful person!" 
+
+
+
+// 2. Tile 2 has a button. 
+// - Create a click event and have the background color of tile 2 change to a shade of green.
+// - BE SURE TO SAVE THE BUTTON FIRST AS A VARIABLE.
+// - Can you see if there's a way to toggle it???
+
+
+// 3. What's this? A totoro? Where's his friends? 
+// - Using createElement, create a friend (an image element).
+// - Once created, set the src of it to the image address.
+// - Append the element to tile 3. 
+// - Need some inspiration? Go to giphy.com, search a character and choose the "stickers".
+
+
+// 4. Oh no! A broken image! We need to fix this.
+// - Set the src for tile 4 to have an image of your favorite TV show.
+
+
+
+
+// These following tasks may require a little bit of digging. 
+// Unsure how to complete the task? Use your resources! Code Nation reference table might help. 
+// Can't find it there? Google search the keywords of what you're trying to do and use web dev documentation! 
+// W3schools, MDN Web Docs, and Stack Overflow all super reliable 😊
+
+
+// 5. Hmm. Looks like the font is too small. 
+// - Update the font-size of tile 5 to 35 pixels.
+
+
+// 6. Another sad box! 
+// - Let make this tile 6 feel special and add a gradient background! 
+// - What kind of gradient? What color? YOU CHOOSE!
+
+// 7. Too many squares. 
+// - Let's change tile 7 to have rounded edges - be sure to search for which CSS property does this :)
+
+// 8. Replace the image with your favorite emoji. 
+// - Try going to this site to search: https://emojipedia.org/
+
+// 9. Display a random number in the tile when the button is clicked.
+// - BE SURE TO SAVE THE BUTTON FIRST AS A VARIABLE.
+
+
+
